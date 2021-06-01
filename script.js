8e6ad72fbc19d4aa3d8d2eded9a9c1d671eb4807
Erick Santos <erickosantos.dev@gmail.com>
2021-06-01 16:50:28 -0300

    3 - Adicione a cor preta como a primeira cor da paleta de cores.

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-let colors = document.getElementsByClassName('color');
+const colors = document.getElementsByClassName('color');
 
 // Função cor aleatória
 function randomColor() {
-  let colorR = Math.ceil(Math.random() * 255);
-  let colorG = Math.ceil(Math.random() * 255);
-  let colorB = Math.ceil(Math.random() * 255);
+  const colorR = Math.ceil(Math.random() * 255);
+  const colorG = Math.ceil(Math.random() * 255);
+  const colorB = Math.ceil(Math.random() * 255);
   return `rgb(${colorR}, ${colorG}, ${colorB})`;
 }
 // Colocando uma cor para cada div da paleta
 function setColors() {
-  for (let color of colors) {
-    color.style.backgroundColor = randomColor();
+  for (let i = 0; i < colors.length; i += 1) {
+    if (i === 0) {
+      colors[i].style.backgroundColor = 'black';
+    } else {
+      colors[i].style.backgroundColor = randomColor();
+    }
   }
 }
 setColors();
